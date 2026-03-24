46c6cd66302049de9fd033834544cc7356d0f147
siraitjr <rioronaldsirait@gmail.com>
2026-03-24 20:03:49 +0700

    update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.koperasikitagodangulu"
         minSdk = 21
         targetSdk = 34
-        versionCode = 32
-        versionName = "4.3"
+        versionCode = 33
+        versionName = "4.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
