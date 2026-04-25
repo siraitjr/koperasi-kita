e94b3e8cd4ae93c9b219b8d332ee6ddf37bf416b
siraitjr <rioronaldsirait@gmail.com>
2026-04-25 19:42:13 +0700

    chore: bump version to 5.4 (53)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.koperasikitagodangulu"
         minSdk = 21
         targetSdk = 34
-        versionCode = 38
-        versionName = "4.9"
+        versionCode = 53
+        versionName = "5.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
