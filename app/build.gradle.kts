a95357da8663cfed732b97ea0a20ce02d30a2901
siraitjr <rioronaldsirait@gmail.com>
2026-04-26 19:14:36 +0700

    chore: bump version code to [54] and version name to [5.5]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.koperasikitagodangulu"
         minSdk = 21
         targetSdk = 34
-        versionCode = 53
-        versionName = "5.4"
+        versionCode = 54
+        versionName = "5.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
